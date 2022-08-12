2befa3c3069ebd9f807e371674a97435abb27d10
cristicoss <64747154+cristicoss@users.noreply.github.com>
2022-08-12 18:21:09 +0200

    blue bg

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { greetUser } from '$utils/greet';
 window.Webflow ||= [];
 window.Webflow.push(() => {
   const name = 'Cristian Costea Claudiu';
+  document.body.style.backgroundColor = 'blue';
   greetUser(name);
 });
